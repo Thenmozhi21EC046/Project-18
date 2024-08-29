639d4f467cf2cfdd7bcc89e26930396e09be4e48
Thenmozhi21EC046 <146418663+Thenmozhi21EC046@users.noreply.github.com>
2024-08-29 11:56:33 +0530

    Add files via upload

---
+++ WebprjctApplication.java
@@ -0,0 +1,13 @@
+package com.staff.webprjct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprjctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprjctApplication.class, args);
+	}
+
+}
